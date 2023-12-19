2120e88628c1cf3a080ae5ab81647339d00db5d7
hhhfriends19 <84479916@qq.com>
2023-12-19 22:36:44 +0800

    finish function _healthFactor in DSCEngine.sol

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -180,6 +180,7 @@ contract DSCEnging is ReentrancyGuard {
         // total collateral VALUE, make sure the value is greater than totaldscminted
         (uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
         uint256 collateralAdjustedForThreshold = (collateralValueInUsd * LIQUIDATION_THRESHOLD) / LIQUIDATION_PRECISION;
+        return (collateralAdjustedForThreshold * PRECISION) / totalDscMinted;
         //return (collateralValueInUsd / totalDscMinted);
     }
 
