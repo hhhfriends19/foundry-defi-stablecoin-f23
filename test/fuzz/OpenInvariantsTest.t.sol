d210c5e3a1bdc32f9feee6468b75067c913e67b0
hhhfriends19 <84479916@qq.com>
2024-02-02 11:35:10 +0800

    get the value of all the collateral in the prococol in OpenInvariantsTest.t.sol

--- test/fuzz/OpenInvariantsTest.t.sol
+++ test/fuzz/OpenInvariantsTest.t.sol
@@ -15,16 +15,25 @@ import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract OpenInvariantsTest is StdInvariant, Test {
     DeployDSC deployer;
     DSCEngine dsce;
     DecentralizedStableCoin dsc;
     HelperConfig config;
+    address weth;
+    address wbtc;
 
     function setUp() external {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
+        (,, weth, wbtc,) = config.activeNetworkConfig();
         targetContract(address(dsce));
+    }
+
+    function invariant_protocolMustHaveMoreValueThanTotalSupply() public view {
+        // get the value of all the collateral in the protocol <- use HelperConfig to get it(weth,wbtc)
+        // compare it to all the debt (dsc)
     }
 }
