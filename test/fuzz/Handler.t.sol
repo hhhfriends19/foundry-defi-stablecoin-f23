d97afc8435989da705bf9e8210137d7cdb108c82
hhhfriends19 <84479916@qq.com>
2024-02-06 20:17:29 +0800

    add script Handler.t.sol

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier:MIT
 
+// Handler is going to narrow down the way we call function
+
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+
+contract Handler is Test {
+    constructor() {}
+}
