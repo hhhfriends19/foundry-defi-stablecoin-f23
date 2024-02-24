c7c8a54d8d9a9f8469bca99587f28c399066988a
hhhfriends19 <84479916@qq.com>
2024-02-24 23:32:17 +0800

    Modify redeemCollateral in Handler.t.sol

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -44,7 +44,10 @@ contract Handler is Test {
     function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
         ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
         uint256 maxCollateralToRedeem = dsce.getCollateralBalanceOfUser(address(collateral), msg.sender);
-        amountCollateral = bound(amountCollateral, 1, maxCollateralToRedeem);
+        amountCollateral = bound(amountCollateral, 0, maxCollateralToRedeem);
+        if (amountCollateral == 0) {
+            return;
+        }
         dsce.redeemCollateral(address(collateral), amountCollateral);
     }
 
